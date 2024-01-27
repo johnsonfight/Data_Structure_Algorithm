ded4304263bb13472369934d1a8d30c8614a0727
Johnson Nieh <johnsonfight@gmail.com>
2024-01-27 15:30:28 +0800

    add 167 two_pointer.cpp

---
+++ 167. Two Sum II - Input Array Is Sorted/two_pointer.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0;
+        int r = numbers.size() - 1;
+        vector<int> output;
+
+        while (l < r){
+            if (numbers[l] + numbers[r] < target){
+                l += 1;
+            } else if (numbers[l] + numbers[r] == target){
+                output.push_back(l + 1);
+                output.push_back(r + 1);
+                return output;
+            } else if (numbers[l] + numbers[r] > target){
+                r -= 1;
+            }
+        }
+        return output;
+    }
+};
