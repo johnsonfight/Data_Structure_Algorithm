db9e5b118d3718babc1ce58228e93728c2ced955
Johnson Nieh <johnsonfight@gmail.com>
2024-01-27 17:14:25 +0800

    modify self.DA init and impacted functions

--- 167. Two Sum II - Input Array Is Sorted/two_pointer.cpp
+++ 167. Two Sum II - Input Array Is Sorted/two_pointer.cpp
@@ -1,24 +1,36 @@
-#include <vector>
-using namespace std;
+class DynamicArray:
+    
+    def __init__(self, capacity: int):
+        self.DA = [None] * capacity
+        self.size = 0
+        self.capacity = capacity
 
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int l = 0;
-        int r = numbers.size() - 1;
-        vector<int> output;
+    def get(self, i: int) -> int:
+        return self.DA[i]
 
-        while (l < r){
-            if (numbers[l] + numbers[r] < target){
-                l += 1;
-            } else if (numbers[l] + numbers[r] == target){
-                output.push_back(l + 1);
-                output.push_back(r + 1);
-                return output;
-            } else if (numbers[l] + numbers[r] > target){
-                r -= 1;
-            }
-        }
-        return output;
-    }
-};
+    def set(self, i: int, n: int) -> None:
+        if self.DA[i] == None:
+            self.size += 1
+        self.DA[i] = n
+        
+    def pushback(self, n: int) -> None:
+        if self.size == self.capacity:
+            self.resize()
+
+        self.DA[self.size] = n
+        self.size += 1
+
+    def popback(self) -> int:
+        self.size -= 1
+        return self.DA[self.size]
+
+    def resize(self) -> None:
+        self.capacity = self.capacity * 2
+        self.DA += [None] * self.capacity
+
+    def getSize(self) -> int:
+        return self.size
+    
+    def getCapacity(self) -> int:
+        print(self.DA)
+        return self.capacity
